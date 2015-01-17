bb12821c11463283d7e107245b47f516f491b6b3
Max Yankov <golergka@gmail.com>
2015-01-17 16:33:59 +0200

    Changed div position to percentages

--- app.js
+++ app.js
@@ -4,21 +4,13 @@
 	var circle = document.getElementById("circle");
 	function update() {
 		if (typeof OrientationProvider !== 'undefined' && !!OrientationProvider) {
-			/*
-			var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-			*/
-			var w = screen.width;
-			var h = screen.height;
-			document.getElementById("screen-size").innerHTML = "width: " + w + " height: " + h;
 			var orientationString = OrientationProvider.getOrientation();
 			readout.innerHTML = "acceleration: " + orientationString;
 			var orientation = eval(orientationString);
-			var r = circle.style.borderRadius;
 			var l = (-orientation[0] + 10) / 20;
 			var t = (orientation[1] + 10) / 20;
-			circle.style.left = l * (w - r) + "px";
-			circle.style.top = t * (h - r) + "px";
+			circle.style.left = l + "%";
+			circle.style.top = t + "%";
 			document.getElementById("debug").innerHTML = "l: " + l + " t: " + t + " circle.style.left: " + circle.style.left + " circle.style.top: " + circle.style.top;
 		}
 		requestAnimationFrame(update);
