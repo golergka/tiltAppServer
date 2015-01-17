e8e5308b33332b4b8c91bf0a77b6e18c01bdc68f
Max Yankov <golergka@gmail.com>
2015-01-17 16:03:36 +0200

    Changed calculation

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
 			var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 			var r = circle.style.borderRadius;
+			var l = (orientation[0] + 10) / 20;
+			var t = (orientation[1] + 10) / 20;
 			circle.style.left = (r + orientation[0] * (w - 2*r)) + "px";
 			circle.style.top = (r + orientation[1] * (h - 2*r)) + "px";
 		}
