b14e28b58013bfc7935e509e96400cb1bce35740
Max Yankov <golergka@gmail.com>
2015-01-17 16:45:24 +0200

    Release ready

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 	"use strict";
 	var readout = document.getElementById("readout");
 	var circle = document.getElementById("circle");
+	var button = document.getElementById("start");
 	function update() {
 		if (typeof OrientationProvider !== 'undefined' && !!OrientationProvider) {
-			var orientationString = OrientationProvider.getOrientation();
-			readout.innerHTML = "acceleration: " + orientationString;
-			var orientation = eval(orientationString);
+			var orientation = JSON.parse(OrientationProvider.getOrientation());
+			readout.innerHTML = "coordinations: " + orientation[0].toFixed(2) + ", " + orientation[1].toFixed(2);
 			var l = (-orientation[0] + 10) / 20;
 			var t = (orientation[1] + 10) / 20;
 			circle.style.left = 100 * l + "%";
 			circle.style.top = 100 * t + "%";
-			document.getElementById("debug").innerHTML = "l: " + l + " t: " + t + " circle.style.left: " + circle.style.left + " circle.style.top: " + circle.style.top;
 		}
 		requestAnimationFrame(update);
 	}
-	update();
+	button.onclick = function() {
+		button.parentElement.removeChild(button);
+		update();
+	};
 }());
