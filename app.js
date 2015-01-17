5072476ac02285a7943fa1dc0ed6dcf32200049e
Max Yankov <golergka@gmail.com>
2015-01-17 15:58:30 +0200

    Circle positioned by coordinates

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 (function() {
 	"use strict";
 	var readout = document.getElementById("readout");
+	var circle = document.getElementById("circle");
 	function update() {
 		if (typeof OrientationProvider !== 'undefined' && !!OrientationProvider) {
-			readout.innerHTML = "coodrinates: " + OrientationProvider.getOrientation();
+			var orientationString = OrientationProvider.getOrientation();
+			readout.innerHTML = "coodrinates: " + orientationString;
+			var orientation = eval(orientationString);
+			var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+			var r = circle.style.borderRadius;
+			circle.style.left = (r + orientation[0] * (w - 2*r)) + "px";
+			circle.style.top = (r + orientation[1] * (h - 2*r)) + "px";
 		}
 		requestAnimationFrame(update);
 	}
